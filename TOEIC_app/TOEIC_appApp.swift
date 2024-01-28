c29817c6f6cd009e355c224ae50101d26e4a280f
masa350z <msum4524@gmail.com>
2024-01-28 21:03:47 +0900

    Initial Commit

---
+++ TOEIC_app/TOEIC_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOEIC_appApp.swift
+//  TOEIC_app
+//
+//  Created by Masaumi TANIGUCHI on 2024/01/28.
+//
+
+import SwiftUI
+
+@main
+struct TOEIC_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
